7e591d268787aa4b14a231ef17402b22e38099ce
tim.mac <sses79@hotmail.com>
2016-11-13 13:47:33 +0000

    laravel admin panel

--- config/app.php
+++ config/app.php
@@ -178,6 +178,11 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        Appzcoder\LaravelAdmin\LaravelAdminServiceProvider::class,
+        // For crud generator & html
+        Appzcoder\CrudGenerator\CrudGeneratorServiceProvider::class,
+        Collective\Html\HtmlServiceProvider::class,
+
     ],
 
     /*
@@ -225,6 +230,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Form' => Collective\Html\FormFacade::class,
+        'HTML' => Collective\Html\HtmlFacade::class,
 
     ],
 
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,20 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::group(['prefix' => 'admin'], function () {
+   Route::get('/', ['uses' => 'Admin\AdminController@index']);
 });
+
+Route::get('admin', 'Admin\AdminController@index');
+Route::get('admin/give-role-permissions', 'Admin\AdminController@getGiveRolePermissions');
+Route::post('admin/give-role-permissions', 'Admin\AdminController@postGiveRolePermissions');
+Route::resource('admin/roles', 'Admin\RolesController');
+Route::resource('admin/permissions', 'Admin\PermissionsController');
+Route::resource('admin/users', 'Admin\UsersController');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index');
